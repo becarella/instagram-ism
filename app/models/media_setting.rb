5014367996d6923b7f833a0f03b174a4b0e8e2bd
Becky Carella <becarella@gmail.com>
2013-07-14 21:54:24 -0400

    fixed media setting

--- app/models/media_setting.rb
+++ app/models/media_setting.rb
@@ -1,36 +1,14 @@
-ActiveAdmin.register Media do
-  
-  filter :author_username_contains, :as => :string
-  filter :author_name_contains, :as => :string
-  filter :tag_list_contains, :as => :string
-  filter :type, :as => :check_boxes, :collection => proc { Media.select("type").group("type").all.collect(&:type) }
-  
-  # config.batch_actions = false
-  
-  actions :index, :show, :update, :destroy
-  
-  
-  config.sort_order = "posted_at_desc"
-  index :as => :grid, :columns => 5 do |media|
-    div :for => media do
-      resource_selection_cell media
-      div link_to(image_tag("#{media.content_url}", :width => 200), admin_medium_path(:id => media), :target => '_blank')
-    end
-  end
-  
-  show do
-    render "form"
-  end
-  
-  controller do
-    with_role :admin
-    
-    def update
-      params[:media] ||= {}
-      params[:media].merge!(params.delete(:instagram_photo)) if params[:instagram_photo]
-      super update
-    end
-  end
+# == Schema Information
+#
+# Table name: media_settings
+#
+#  id     :integer          not null, primary key
+#  source :string(255)
+#  key    :string(255)
+#  value  :string(255)
+#
 
+class MediaSetting < ActiveRecord::Base
   
+  attr_accessible :value
 end
